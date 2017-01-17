a2c44c9325f48280d14004a5fea70e9903901f0d
cromerMBPr <charlie.romer@gmail.com>
2017-01-16 21:31:13 -0500

    updated RelayControl
    cleaned up the status topic string creating

--- Home Automation/Sketches/RelayControl/RelayControl.ino
+++ Home Automation/Sketches/RelayControl/RelayControl.ino
@@ -18,23 +18,32 @@
     along with RelayControl.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+	This is a simple MQTT relay/light controller program for the ESP8266.
+	By sending a '1' or '0' to the relayTopic the relayPin can be toggled
+	on or off. This program also posts a status update to the status topic
+	which is the relayTopic plus "/status" (ex. if the relayTopic
+	is "/home/light" then the statusTopic would be "home/light/status")
+*/
+
 #include "ESPHelper.h"
 
-#define STATUS "/status"	//dont change this - this is for the status topic which is whatever your mqtt topic is plus status (ex /home/light/status)
 #define TOPIC "/your/mqtt/topic"
+#define STATUS TOPIC "/status"	//dont change this - this is for the status topic which is whatever your mqtt topic is plus /status (ex /home/light/status)
+
 #define NETWORK_HOSTNAME "YOUR OTA HOST NAME"
 #define OTA_PASSWORD "YOUR OTA PASSWORD"
+
 #define RELAY_PIN 3		//rx pin on esp
 #define BLINK_PIN 1		//tx/led on esp-01
 
 
-
 char* relayTopic = TOPIC;
+char* statusTopic = STATUS;
 char* hostnameStr = NETWORK_HOSTNAME;
+
 const int relayPin = RELAY_PIN;
 const int blinkPin = BLINK_PIN;		//tx pin on esp
-
-String statusTopic;
 
 //set this info for your own network
 netInfo homeNet = {.name = "NETWORK NICKNAME", .mqtt = "YOUR MQTT-IP", .ssid = "YOUR SSID", .pass = "YOUR NETWORK PASS"};
@@ -42,11 +51,6 @@ netInfo homeNet = {.name = "NETWORK NICKNAME", .mqtt = "YOUR MQTT-IP", .ssid = "
 ESPHelper myESP(&homeNet);
 
 void setup() {
-
-	//create the status topic string
-	statusTopic = relayTopic;
-	statusTopic.concat(STATUS);
-
 	//setup ota
 	myESP.OTA_enable();
 	myESP.OTA_setPassword(OTA_PASSWORD);
@@ -61,7 +65,7 @@ void setup() {
 	
 
 	pinMode(relayPin, OUTPUT);
-    delay(500);
+    delay(100);
 }
 
 
@@ -76,20 +80,16 @@ void loop(){
 void callback(char* topic, byte* payload, unsigned int length) {
 	String topicStr = topic;
 
-	//convert the status topic String to a char* so the mqtt lib can use it
-	char replyTopic[50];
-	statusTopic.toCharArray(replyTopic, 50);
-
 	//if the payload from mqtt was 1, turn the relay on and update the status topic with 1
 	if(payload[0] == '1'){
 		digitalWrite(relayPin, HIGH);
-		myESP.publish(replyTopic, "1",true);
+		myESP.publish(statusTopic, "1",true);
 	}
 
 	//else turn the relay off and update the status topic with 0
 	else if (payload[0] == '0'){
 		digitalWrite(relayPin, LOW);
-		myESP.client.publish(replyTopic, "0", true);
+		myESP.client.publish(statusTopic, "0", true);
 	}
 
 }
